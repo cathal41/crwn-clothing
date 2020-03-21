65bdb87f4064d4af6e05a54270202041c053adb4
Cathal McCabe <cathalmccabe@Cathals-MBP.home>
2020-03-21 16:18:13 +0000

    added basic routing to hats page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Switch } from "react-router-dom";
+
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatsPage = () => (
+  <div>
+      <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
-    </div>
+    <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+    </Switch>
+</div>
   );
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+
 import './homepage.styles.scss'
-import Directory from '../../components/diretcory/directory.component';
+import Directory from '../../components/directory/directory.component';
+
 
 const HomePage = () => {
     return (
