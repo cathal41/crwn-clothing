278dcfad13f12c6c7425f30a7b0ee2b2327ebc88
Cathal McCabe <cathalmccabe@Cathals-MBP.home>
2020-03-30 09:00:11 +0100

    can now add mutlibes of same item and new shopdata file

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,8 @@ import {CART_ACTION_TYPES} from './cart.types';
 export const toggleCartHidden = () =>({
     type: CART_ACTION_TYPES.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item =>({
+    type: CART_ACTION_TYPES.ADD_ITEM,
+    payload: item
+});
