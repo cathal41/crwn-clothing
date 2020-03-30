c3193d5fee0a33b9b4a8f0a0d6c2d981155b79b3
Cathal McCabe <cathalmccabe@Cathals-MBP.home>
2020-03-30 17:49:26 +0100

    added add and remove at checkout page

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -8,3 +8,13 @@ export const addItem = item =>({
     type: CART_ACTION_TYPES.ADD_ITEM,
     payload: item
 });
+
+export const removeItem = item =>({
+    type: CART_ACTION_TYPES.REMOVE_ITEM,
+    payload: item
+});
+
+export const clearItemFromCart = item =>({
+    type: CART_ACTION_TYPES.CLEAR_ITEM_FROM_CART,
+    payload: item
+});
